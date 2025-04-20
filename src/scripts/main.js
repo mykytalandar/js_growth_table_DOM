daf9fd819efcb7b978d591903edce31a756d3198
Mykyta Landar <mykytalandar@gmail.com>
2025-04-20 04:18:15 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,76 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+
+const buttons = document.querySelectorAll('.button');
+
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+
+const appendColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+
+function updateButtons() {
+  const countRow = tbody.querySelectorAll('tr').length;
+  const countColumn = tbody.querySelector('tr').querySelectorAll('td').length;
+
+  appendRowButton.disabled = countRow >= 10;
+  removeRowButton.disabled = countRow <= 2;
+  appendColumnButton.disabled = countColumn >= 10;
+  removeColumnButton.disabled = countColumn <= 2;
+}
+
+for (const button of buttons) {
+  button.addEventListener('click', (e) => {
+    const targetButton = e.target;
+
+    if (targetButton.classList.contains('append-row')) {
+      const tr = document.createElement('tr');
+
+      const trOnTable = document.querySelector('tr');
+      const countOfTd = trOnTable.cells.length;
+
+      for (let i = 0; i < countOfTd; i++) {
+        const td = document.createElement('td');
+
+        tr.append(td);
+      }
+
+      tbody.appendChild(tr);
+
+      updateButtons();
+    }
+
+    if (targetButton.classList.contains('remove-row')) {
+      if (tbody.lastElementChild) {
+        tbody.removeChild(tbody.lastElementChild);
+      }
+
+      updateButtons();
+    }
+
+    if (targetButton.classList.contains('append-column')) {
+      const alltrOnTable = tbody.querySelectorAll('tr');
+
+      for (const tr of alltrOnTable) {
+        const td = document.createElement('td');
+
+        tr.append(td);
+      }
+
+      updateButtons();
+    }
+
+    if (targetButton.classList.contains('remove-column')) {
+      const allRows = tbody.querySelectorAll('tr');
+
+      for (const row of allRows) {
+        if (row.cells.length > 0) {
+          row.deleteCell(-1);
+        }
+      }
+
+      updateButtons();
+    }
+  });
+}
